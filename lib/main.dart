939677414abd75d934bab5952fe84dbf41b0de37
sup <pacheer555@gmail.com>
2020-07-05 17:35:36 +0700

    cleanup code

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      title: 'Simple Platform Channel',
       theme: ThemeData(
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
--- lib/platform_channel.dart
+++ lib/platform_channel.dart
@@ -5,7 +5,6 @@ import 'package:flutter/services.dart';
 class PlatformChannel {
   static const MethodChannel _methodChannel = const MethodChannel('sample.test.platform/text');
   static const EventChannel _eventChannel = const EventChannel('sample.test.platform/number');
-  Stream<int> _onNumberChanged;
 
   /// MethodChannel com.test.platform/text
   Future<String> getStringReturnFromPlatform(String text) async {
